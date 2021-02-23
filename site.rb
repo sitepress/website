c2f8ec456eb94215fd18168711d4d0fbef02e974
Brad Gessler <bradgessler@gmail.com>
2021-02-22 22:35:31 -0800

    Remove old site.rb config file

--- site.rb
+++
@@ -1,5 +0,0 @@
-# site.manipulate do |resource|
-#   if !resource.data.key? "layout" and resource.mime_type == "text/html"
-#     resource.data["layout"] = "layouts/layout.html.haml"
-#   end
-# end
