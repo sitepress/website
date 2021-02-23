3ebab11cbca36dcbd99b1ce49cc7c59ebb9a4d06
Brad Gessler <bradgessler@gmail.com>
2021-02-22 22:33:55 -0800

    Move assets into asset pipeline

--- helpers/page_helper.rb
+++ helpers/page_helper.rb
@@ -1,13 +1,14 @@
 module PageHelper
-  def link_to_page(page)
-    link_to page.data.fetch("title", page.request_path), page.request_path
+  def link_to_page(page, text=nil, *args, key: "title", **kwargs)
+    text ||= page.data.fetch(key, page.request_path)
+    link_to text, page.request_path, *args, **kwargs
   end
 
-  def link_to_if_current(text, page, active_class: "active")
+  def link_to_if_current(text, page, *args, active_class: "active", **kwargs)
     if page == current_page
-      link_to text, page.request_path, class: active_class
+      link_to text, page.request_path, *args, class: active_class, **kwargs
     else
-      link_to text, page.request_path
+      link_to text, page.request_path, *args, **kwargs
     end
   end
 end
